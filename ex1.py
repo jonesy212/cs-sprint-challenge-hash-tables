e61eb1c9f89d8abad9148164ed6bbbe11e20323f
Marcus Jones <indelibleink84@gmail.com>
2020-07-21 20:27:56 -0500

    worked on ex1 and ex3

---
+++ ex1.py
@@ -0,0 +1,34 @@
+def get_indices_of_item_weights(weights, length, limit):
+    """
+    YOUR CODE HERE
+    """
+    # create a list for weights:
+    weights = [] * length
+    length = len(weights)
+
+def hash(self, key):
+    
+    length = len(self.weights)
+    # if the two items are equal to the limit
+    for item in range(self.weights):
+        zeroth = 0
+        first = 0
+        if self.limit == first + zeroth:
+            True
+            print((first, zeroth))
+        # if item is bigger than 2nd item, 
+        if item > item:
+            item = first
+        else:
+            # else place item in zeroth 
+            item = zeroth
+        # if two of the weights equal the limit
+        if self.weights[item] + self.weights[item] == self.limit:
+            item
+        # if zero is greater than first switch places so the
+        # smaller number is in the zeroth spot
+        if zeroth > first:
+            first == zeroth
+        print(length, (zeroth, first))
+    return None
+# get_indices_of_item_weights()
